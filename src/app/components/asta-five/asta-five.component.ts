baee7fe1049188d43afe1b29a56ed36711c50661
duxard <duxard@gmail.com>
2021-02-20 21:48:43 +0300

    update

--- src/app/components/asta-five/asta-five.component.ts
+++ src/app/components/asta-five/asta-five.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import {FormGroup,FormControl,Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-asta-five',
@@ -7,28 +7,17 @@ import {FormGroup,FormControl,Validators} from '@angular/forms';
   styleUrls: ['./asta-five.component.scss']
 })
 export class AstaFiveComponent implements OnInit {
-  form: FormGroup
 
+  formControls = {
+    title: '',
+    descr: ''
+  };
   ngOnInit() {
-    this.form = new FormGroup({
-      email: new FormControl('', [
-        Validators.email,
-        Validators.required
-      ]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6)
-      ])
-    })
+
   }
 
-  submit() {
-    if (this.form.valid) {
-      console.log('Form: ', this.form)
-      const formData = {...this.form.value}
-
-      console.log('Form Data:', formData)
-    }
+  onSubmit(){
+    console.log(this.formControls);
   }
 }
 
